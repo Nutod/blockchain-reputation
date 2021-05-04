df3a0913710e68d0723d14f6f8fcf31a9211f5d4
Nutod <alukodotun@gmail.com>
2021-05-04 21:18:33 +0700

    Added new class methods

--- src/dlt/block.ts
+++ src/dlt/block.ts
@@ -1,6 +1,12 @@
 import cryptoHash from 'src/lib/crypto-hash'
-import hexToBinary from 'hex-to-binary'
 import { GENESIS_DATA } from 'src/config'
+
+interface BlockItem {
+  lastHash: string
+  hash: string
+  data: any[]
+  timestamp: number
+}
 
 export default class Block {
   lastHash: string
@@ -23,5 +29,24 @@ export default class Block {
     this.hash = hash
     this.data = data
     this.timestamp = timestamp
+  }
+
+  static genesis() {
+    return new this(GENESIS_DATA)
+  }
+
+  static mineBlock({ lastBlock, data }: { lastBlock: BlockItem; data: any[] }) {
+    // do some things here
+
+    //  lastHash: string
+    //  hash: string
+    //  data: any[]
+    //  timestamp: number
+
+    const lastHash = lastBlock.hash
+    const timestamp = Date.now()
+    const hash = cryptoHash(timestamp, lastHash, data)
+
+    return new this({ lastHash, hash, data, timestamp })
   }
 }
--- src/dlt/index.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'hex-to-binary' {}
-
-declare function hexToBinary(s: string): string
-
-declare const lookup: {
-  [id: string]: string
-}

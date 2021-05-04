ee65fa4d341fdc05601d375bf21699aecc416c84
Nutod <alukodotun@gmail.com>
2021-05-04 22:37:01 +0700

    Updated blockchain main class

--- src/dlt/blockchain.ts
+++ src/dlt/blockchain.ts
@@ -1,4 +1,6 @@
 import Block, { IBlock } from './block'
+import cryptoHash from 'src/lib/crypto-hash'
+import { REWARD_INPUT, REWARD } from 'src/config'
 
 export class Blockchain {
   constructor(public chain: IBlock[] = [Block.genesis()]) {}
@@ -10,5 +12,98 @@ export class Blockchain {
     this.chain.push(newBlock)
 
     return newBlock
+  }
+
+  static isValid(chain: IBlock[]) {
+    // Valid rules implemented here
+    if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())) {
+      return false
+    }
+
+    //  Enforce the rules for the chain
+    //  This portion is still using nonce and difficulty
+    for (let i = 1; i < chain.length; i++) {
+      const { timestamp, hash, lastHash, data } = chain[i]
+
+      const actualLastHash = chain[i - 1].hash
+
+      if (lastHash !== actualLastHash) {
+        console.log('hash values do not match')
+        return false
+      }
+
+      const validatedHash = cryptoHash(timestamp, data, lastHash)
+
+      if (validatedHash !== hash) {
+        console.log('hash values do not match')
+        return false
+      }
+    }
+
+    return true
+  }
+
+  replaceChain(
+    chain: IBlock[],
+    validateTransactions?: boolean,
+    successCallback?: () => void,
+  ) {
+    if (chain.length <= this.chain.length) {
+      console.log('Chain will be preserved as incoming chain is not longer')
+      return
+    }
+
+    if (!Blockchain.isValid(chain)) {
+      return
+    }
+
+    if (validateTransactions && !this.validTransactionData({ chain })) {
+      console.error('The incoming chain has invalid data')
+      return
+    }
+
+    if (successCallback) {
+      successCallback()
+    }
+
+    // If nothing goes wrong, then you can replace
+    this.chain = chain
+  }
+
+  validTransactionData({ chain }: { chain: IBlock[] }) {
+    for (let i = 1; i < chain.length; i++) {
+      const block = chain[i]
+      let rewardTransactionCount = 0
+      const transactionSet = new Set()
+
+      for (let transaction of block.data) {
+        //   if (transaction.input.address === REWARD_INPUT.address) {
+        //     rewardTransactionCount += 1
+
+        //     if (rewardTransactionCount > 1) {
+        //       console.error('Reward exceeds required limit of 1')
+        //       return false
+        //     }
+
+        //     if (Object.values(transaction.outputMap)[0] !== REWARD) {
+        //       console.error('Reward exceeds default config')
+        //       return false
+        //     }
+        //   }
+        if (transaction.input.address !== REWARD_INPUT.address) {
+          if (!Transaction.validTransaction(transaction)) {
+            console.error('Transaction not valid')
+            return false
+          }
+
+          if (transactionSet.has(transaction)) {
+            console.error('An identical transaction was detected')
+            return false
+          } else {
+            transactionSet.add(transaction)
+          }
+        }
+      }
+    }
   }
 }

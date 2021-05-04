2f5c7e7f6e01bcd1a7b78a236ed3d469b38b89fe
Nutod <alukodotun@gmail.com>
2021-05-04 20:45:13 +0700

    Added new lib functions

---
+++ src/lib/keys.ts
@@ -0,0 +1,21 @@
+import Elliptic from 'elliptic';
+import cryptoHash from './crypto-hash';
+
+const EC = Elliptic.ec;
+const ec = new EC('secp256k1');
+
+export default function verifySignature({
+  publicKey,
+  data,
+  signature,
+}: {
+  publicKey: string;
+  data: any;
+  signature: any;
+}) {
+  // Retrieving the private from the public
+  const keyFromPublic = ec.keyFromPublic(publicKey, 'hex');
+
+  // Return a Boolean as you might expect
+  return keyFromPublic.verify(cryptoHash(data), signature);
+}

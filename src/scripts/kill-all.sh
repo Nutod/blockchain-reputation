143b4987ac7c40f8a9aea48e93edceedac51d8b1
Nutod <alukodotun@gmail.com>
2021-05-06 06:59:37 +0700

    Updated start script

--- src/scripts/kill-all.sh
+++ src/scripts/kill-all.sh
@@ -4,5 +4,6 @@ cd ../_data
 
 while read line
 do
+   echo $line
    npx kill-port $line
 done < <(tail -n +2 node-ports.csv)
--- src/scripts/start-peers.sh
+++ src/scripts/start-peers.sh
@@ -3,9 +3,29 @@
 # Chnage the directory
 cd ../_data
 
-# PORT=5001 npm run dev-peer
+# array=( 5001 5002 5003 )
+# for i in "${array[@]}"
+# do
+# 	echo $i
+#    PORT=$i npm run dev-peer
+# done
+
+# while read line
+# do
+#    echo $line
+#    PORT=$line npm run dev-peer
+# done < <(tail -n +2 node-ports.csv)
+
+cust_func(){
+  #wget -q "$1"
+  PORT=$line npm run dev-peer
+  echo "Running background? $line"
+}
+ 
 while read line
 do
-   echo $line
-   # start the server here
+   cust_func "$line" &
 done < <(tail -n +2 node-ports.csv)
+ 
+wait
+echo "All peers"
